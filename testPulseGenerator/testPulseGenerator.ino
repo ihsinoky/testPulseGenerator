ed81e97c32385d1daa00a0e57a723ecef9e02f99
yuta <subscribe.y.konishi@gmail.com>
2018-10-06 20:54:19 +0900

    Added controller.

--- testPulseGenerator/testPulseGenerator.ino
+++ testPulseGenerator/testPulseGenerator.ino
@@ -1,37 +1,20 @@
 #include "TestSetting.h"
 #include "TestSequence.h"
+#include "Controller.h"
 
-int testcount;
+Controller ctrl;
 
 void setup()
 {
-  // put your setup code here, to run once:
-  for (int i = 0; i < SETTING_COUNT; i++)
-  {
-    pinMode(testsetting[i].p.portNum, OUTPUT);
-  }
-  randomSeed(analogRead(0));
-  Serial.begin(115200);
-  Serial.print("Initialized\n");
-  testcount = 0;
+  ctrl.init();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  /* Create test sequence */
-  for (int i = 0; i < SETTING_COUNT; i++)
+  byte ctrlCode[1];
+  if (Serial.readBytes(ctrlCode, 1) != 0)
   {
-    seqs[i] = TestSequence::toSequence(testsetting[i], &workspace[i]);
+    ctrl.transit(ctrlCode[0]);
   }
-  TestSequence::sort();
-  TestSequence::relativize();
-  TestSequence::printSequences(testcount);
-  for (int i = 0; i < SETTING_COUNT; i++)
-  {
-    delay(seqs[i].delayTime);
-    digitalWrite(seqs[i].p.portNum, seqs[i].p.state);
-  }
-  delay(3000);
-  testcount++;
+  ctrl.perform();
 }
