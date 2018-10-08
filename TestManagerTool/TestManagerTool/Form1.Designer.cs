ac5b3ff3c3a8cd4129b7b3e455ec14a33e13b071
yuta <subscribe.y.konishi@gmail.com>
2018-10-08 21:44:32 +0900

    Added receiving function.

--- TestManagerTool/TestManagerTool/Form1.Designer.cs
+++ TestManagerTool/TestManagerTool/Form1.Designer.cs
@@ -28,34 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.portComboBox = new System.Windows.Forms.ComboBox();
+            this.ComOpenClose = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.ClearLogBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // LoadBtn
             // 
-            this.button1.Location = new System.Drawing.Point(38, 99);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Run";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.LoadBtn.Location = new System.Drawing.Point(38, 99);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoadBtn.TabIndex = 0;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // portComboBox
+            // 
+            this.portComboBox.FormattingEnabled = true;
+            this.portComboBox.Location = new System.Drawing.Point(38, 29);
+            this.portComboBox.Name = "portComboBox";
+            this.portComboBox.Size = new System.Drawing.Size(121, 20);
+            this.portComboBox.TabIndex = 1;
+            // 
+            // ComOpenClose
+            // 
+            this.ComOpenClose.Location = new System.Drawing.Point(177, 29);
+            this.ComOpenClose.Name = "ComOpenClose";
+            this.ComOpenClose.Size = new System.Drawing.Size(75, 23);
+            this.ComOpenClose.TabIndex = 2;
+            this.ComOpenClose.Text = "Open";
+            this.ComOpenClose.UseVisualStyleBackColor = true;
+            this.ComOpenClose.Click += new System.EventHandler(this.ComOpenClose_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(333, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(444, 254);
+            this.textBox1.TabIndex = 3;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(38, 144);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(75, 23);
+            this.StartBtn.TabIndex = 4;
+            this.StartBtn.Text = "Start";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(38, 182);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 5;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // ClearLogBtn
+            // 
+            this.ClearLogBtn.Location = new System.Drawing.Point(702, 289);
+            this.ClearLogBtn.Name = "ClearLogBtn";
+            this.ClearLogBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearLogBtn.TabIndex = 6;
+            this.ClearLogBtn.Text = "Clear Log";
+            this.ClearLogBtn.UseVisualStyleBackColor = true;
+            this.ClearLogBtn.Click += new System.EventHandler(this.ClearLogBtn_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(800, 348);
+            this.Controls.Add(this.ClearLogBtn);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.StartBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ComOpenClose);
+            this.Controls.Add(this.portComboBox);
+            this.Controls.Add(this.LoadBtn);
             this.Name = "Form1";
             this.Text = "Test Manager Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox portComboBox;
+        private System.Windows.Forms.Button ComOpenClose;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Button ClearLogBtn;
     }
 }
 
--- TestManagerTool/TestManagerTool/Form1.cs
+++ TestManagerTool/TestManagerTool/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using System.Reflection;
 using System.Diagnostics;
+using System.IO.Ports;
 
 namespace TestManagerTool
 {
@@ -41,14 +42,88 @@ namespace TestManagerTool
                 StreamReader sr = new StreamReader(resStream);
                 string text = sr.ReadToEnd();
                 File.WriteAllText(arduinoFolder + filename, text);
-                
+            }
+
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                portComboBox.Items.Add(port);
+            }
+            if (portComboBox.Items.Count > 0)
+            {
+                portComboBox.SelectedIndex = 0;
             }
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void LoadBtn_Click(object sender, EventArgs e)
         {
             string arg = "--upload " + System.IO.Directory.GetCurrentDirectory() +"\\"+ arduinoFolder + "testPluseGenerator.ino";
             Process p = Process.Start("arduino", arg);
+        }
+        private void ComOpenClose_Click(object sender, EventArgs e)
+        {
+            if (ComOpenClose.Text == "Open")
+            {
+                serialPort1.BaudRate = 115200;
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.None;
+                serialPort1.PortName = portComboBox.Text;
+                serialPort1.Open();
+                ComOpenClose.Text = "Close";
+                backgroundWorker1.RunWorkerAsync();
+            } else
+            {
+                serialPort1.Close();
+                backgroundWorker1.CancelAsync();
+                ComOpenClose.Text = "Open";
+            }
+        }
+        private delegate void updateTextDelegate(char c);
+        private void UpdateText(char c)
+        {
+            textBox1.AppendText(c.ToString());
+        }
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(true)
+            {
+                try
+                {
+                    if (serialPort1.IsOpen)
+                    {
+                        char c = (char)serialPort1.ReadChar();
+                        Invoke(new updateTextDelegate(UpdateText), c);
+                    }
+
+                    if (backgroundWorker1.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("S");
+        }
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("C");
+        }
+        private void ClearLogBtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
         }
     }
 }
